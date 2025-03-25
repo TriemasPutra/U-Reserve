5646a2bc82dd47ec440a72e16ef8cce912cff0d7
TriemasPutra <triemas.412024018@civitas.ukrida.ac.id>
2025-03-25 21:59:33 +0700

    Quick Fix Middleware

--- src/middleware.js
+++ src/middleware.js
@@ -2,10 +2,10 @@ import { NextResponse } from 'next/server'
 
 export function middleware(request) {
   if (request.nextUrl.pathname !== "/login") {
-    // return NextResponse.redirect(new URL('/login', request.url))
+    return NextResponse.redirect(new URL('/login', request.url))
   }
 }
 
 export const config = {
-  matcher: "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|login).*)"
+  matcher: "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|login|user|admin).*)"
 }
