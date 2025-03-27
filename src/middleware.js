33ae37ae605ebcb6fb6c3300c080084dd757bcbc
Hwayeeon <yoonakim320@gmail.com>
2025-03-27 07:18:54 +0700

    fix middlware.js for redirecting user to login page before they entering the websites

--- src/middleware.js
+++ src/middleware.js
@@ -1,11 +1,18 @@
+import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
 
-export function middleware(request) {
-  if (request.nextUrl.pathname !== "/login") {
-    // return NextResponse.redirect(new URL('/login', request.url))
+export async function middleware(req) {
+  const session = (await cookies()).getAll("session")?.value
+  console.log("session : ", session)
+  if (!session && req.url !== "/login") {
+    const url = req.nextUrl.clone();
+    url.pathname = "/login";
+    return NextResponse.rewrite(url)
+  } else {
+    return NextResponse.next()
   }
 }
 
 export const config = {
-  matcher: "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|login|admin|user).*)"
+  matcher: "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|login).*)"
 }
