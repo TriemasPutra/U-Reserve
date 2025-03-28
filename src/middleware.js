8f6d8e0cd7f73ab72c1a2b1ff7f25d7700f6519e
Hwayeeon <yoonakim320@gmail.com>
2025-03-28 16:06:51 +0700

    initial commit

--- src/middleware.js
+++ src/middleware.js
@@ -16,4 +16,5 @@ export function middleware(request) {
 }
 
 export const config = {
-  matcher: "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml).*)"
+  matcher: "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|admin).*)"
+}
