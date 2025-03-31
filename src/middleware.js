152d3cd110579c34fb47f4acd35329c839e526c0
Hwayeeon <yoonakim320@gmail.com>
2025-03-31 12:59:17 +0700

    delete cookies from the middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,19 +1,18 @@
-import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
 
 export function middleware(request) {
-  // const role = request.cookies.get('role')?.value
-  // const url = request.nextUrl.clone();
-  // if (role !== 'student' && role !== 'admin' && url.pathname !== '/login') {
-  //   url.pathname = "/login";
-  //   return NextResponse.rewrite(url);
-  // } else if (role === 'student') {
-  //   url.pathname = "/user";
-  //   return NextResponse.rewrite(url);
-  // } else if (role === 'admin') {
-  //   url.pathname = "/admin";
-  //   return NextResponse.rewrite(url);
-  // }
+  const role = request.cookies.get('role')?.value
+  const url = request.nextUrl.clone();
+  if (role !== 'student' && role !== 'admin' && url.pathname !== '/login') {
+    url.pathname = "/login";
+    return NextResponse.rewrite(url);
+  } else if (role === 'student') {
+    url.pathname = "/user";
+    return NextResponse.rewrite(url);
+  } else if (role === 'admin') {
+    url.pathname = "/admin";
+    return NextResponse.rewrite(url);
+  }
 }
 
 export const config = {
