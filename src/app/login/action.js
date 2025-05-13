58043d1a2fed41b7fddf5d62efeac9efe9906408
TriemasPutra <triemas.412024018@civitas.ukrida.ac.id>
2025-05-13 11:11:14 +0700

    Dynamic User and Logout

--- src/app/login/action.js
+++ src/app/login/action.js
@@ -2,6 +2,8 @@
 
 import { redirect } from 'next/navigation'
 import { createClient } from '@/utils/supabase/server'
+import { cookies } from "next/headers";
+import { encrypt } from "@/lib/crypt";
 
 export async function login(credentials) {
   const supabase = await createClient()
@@ -29,14 +31,18 @@ export async function login(credentials) {
   // })
   // console.log('\nlogin\n', userData, userError);
 
+  const cookieStore = await cookies();
   const { data: userData, error: userError } = await supabase.auth.signInWithPassword({
     email: user.email,
     password: credentials['password_hash'],
   })
-
+  
   if (userError) {
     return { status: 401, }
-  } else if (userData.user.user_metadata.role === 'admin') {
+  }
+  const encryptedData = encrypt(JSON.stringify(userData.user.user_metadata));
+  cookieStore.set('user', encryptedData, {maxAge: 60*60, path: '/'});
+  if (userData.user.user_metadata.role === 'admin') {
     redirect('/admin');
   } else if (userData.user.user_metadata.role === 'student') {
     redirect('/user');
