8fa0ea72d3e45eb55ac4aab3331c8b2f04692010
Hwayeeon <yoonakim320@gmail.com>
2025-05-17 00:55:29 +0700

    menambahkan fileform-reservation dan text-area untuk form pemesanan ruangan

---
+++ src/app/user/form/page.js
@@ -0,0 +1,9 @@
+import RoomReservationForm from "@/components/form-reservation"
+
+export default function Page() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <RoomReservationForm />
+    </main>
+  )
+}
